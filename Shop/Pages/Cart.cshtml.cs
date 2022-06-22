a582297c259a4f67b0a81f6ce7110e40ac70f4a1
margo <margoshka.ri6@gmail.com>
2022-06-22 10:36:11 +0300

    validate product count

--- Shop/Pages/Cart.cshtml.cs
+++ Shop/Pages/Cart.cshtml.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
 using System.Text.Json;
 
 namespace Shop.Pages
